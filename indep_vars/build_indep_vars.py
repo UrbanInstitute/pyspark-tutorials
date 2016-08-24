1692e39aca565194ef88402a0c53a8d33eddb309
Jeff Levy <jlevy@urban.org>
2016-08-24 17:13:26 -0400

    Updating GLM

--- indep_vars/build_indep_vars.py
+++ indep_vars/build_indep_vars.py
@@ -1,3 +1,11 @@
+"""
+Program written by Jeff Levy (jlevy@urban.org) for the Urban Institute, last revised 8/24/2016.
+Note that this is intended as a temporary work-around until pySpark improves its ML package.
+
+Tested in pySpark 2.0.
+
+"""
+
 def build_indep_vars(df, independent_vars, categorical_vars=None, keep_intermediate=False, summarizer=True):
 
     """
@@ -46,7 +54,7 @@ def build_indep_vars(df, independent_vars, categorical_vars=None, keep_intermedi
 
     #for building the crosswalk between indicies and column names
     if summarizer:
-        param_crosswalk = dict()
+        param_crosswalk = {}
 
         i = 0
         for x in independent_vars:
@@ -62,7 +70,6 @@ def build_indep_vars(df, independent_vars, categorical_vars=None, keep_intermedi
             elif '_index' in x[:-6]:
                 pass
             else:
-                print('in else', i, x)
                 param_crosswalk[i] = x
                 i += 1
         """
@@ -90,11 +97,51 @@ def build_indep_vars(df, independent_vars, categorical_vars=None, keep_intermedi
 class Summarizer(object):
     def __init__(self, param_crosswalk):
         self.param_crosswalk = param_crosswalk
+        self.precision = 4
+        self.screen_width = 57
+        self.hsep = '-'
+        self.vsep = '|'
 
-    def summarize(self, model):
-        coefs = model.coefficients
+    def summarize(self, model, show=True, return_str=False):
+        coefs = list(model.coefficients)
         inter = model.intercept
         tstat = model.summary.tValues
-        stder = model.summary.coefficientStandardError
+        stder = model.summary.coefficientStandardErrors
         pvals = model.summary.pValues
 
+        #if model includes an intercept:
+        if len(coefs) == len(tstat)-1:
+            coefs.insert(0, inter)
+            x = {0:'intercept'}
+            for k, v in self.param_crosswalk.items():
+                x[k+1] = v
+        else:
+            x = self.param_crosswalk
+
+        assert(len(coefs) == len(tstat) == len(stder) == len(pvals))
+
+        p = self.precision
+        h = self.hsep
+        v = self.vsep
+        w = self.screen_width
+
+        coefs = [str(round(e, p)).center(10) for e in coefs]
+        tstat = [str(round(e, p)).center(10) for e in tstat]
+        stder = [str(round(e, p)).center(10) for e in stder]
+        pvals = [str(round(e, p)).center(10) for e in pvals]
+
+        lines = ''
+        for i in range(len(coefs)):
+            lines += str(x[i]).rjust(15) + v + coefs[i] + stder[i] + tstat[i] + pvals[i] + '\n'
+
+        labels = ''.rjust(15) + v + 'Coef'.center(10) + 'Std Err'.center(10) + 'T Stat'.center(10) + 'P Val'.center(10)
+        pad    = ''.rjust(15) + v
+
+        output = """{hline}\n{labels}\n{hline}\n{lines}{hline}""".format(
+                    hline=h*w, 
+                    labels=labels,
+                    lines=lines)
+        if show:
+            print(output)
+        if return_str:
+            return output
