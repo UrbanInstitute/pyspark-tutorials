10bc651c765e700e1f5164006706248c65225873
Jeff Levy <jlevy@urban.org>
2016-08-26 16:59:25 -0400

    Setup

---
+++ indep_vars/setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+setup(name='build_indep_vars.py',
+      version='1.0',
+      url='https://github.com/UrbanInstitute/pyspark-tutorials/tree/master/indep_vars'
+      author='Jeff Levy',
+      autor_email='jlevy@urban.org',
+      py_modules=['build_indep_vars'])
